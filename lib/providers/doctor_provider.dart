44867c34d0b2e6e5596362259d5639589ae1856b
Deep27 <geep2795@gmail.com>
2020-06-10 11:06:05 +0300

    Updated doctor photo.

--- lib/providers/doctor_provider.dart
+++ lib/providers/doctor_provider.dart
@@ -4,12 +4,12 @@ import 'package:mvp_platform/models/doctor.dart';
 class Doctors with ChangeNotifier {
   static final List<Doctor> doctors = [
     Doctor(
-      profession: 'Педиатр',
-      name: 'Рыжова С.П. (к. 120)',
-    ),
+        profession: 'Педиатр',
+        name: 'Рыжова С.П. (к. 120)',
+        photoPath: 'assets/images/doctor-2.jpg'),
     Doctor(
-      profession: 'Кардиолог',
-      name: 'Безруков Сергей Валерьевич',
-    )
+        profession: 'Кардиолог',
+        name: 'Безруков Сергей Валерьевич',
+        photoPath: 'assets/images/doctor-bezrukov.jpg')
   ];
 }

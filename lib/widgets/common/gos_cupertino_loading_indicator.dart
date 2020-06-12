8399ac5131b3c3b86392d0af65ada180cb950da0
Deep27 <geep2795@gmail.com>
2020-06-12 11:31:55 +0300

    Updated calendar.

---
+++ lib/widgets/common/gos_cupertino_loading_indicator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GosCupertinoLoadingIndicator extends StatelessWidget {
+  const GosCupertinoLoadingIndicator({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: const CupertinoActivityIndicator(radius: 25.0),
+    );
+  }
+}
